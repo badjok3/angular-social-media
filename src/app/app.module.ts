31f7bbef7b27ed828301c9e8724eb1387be8ab7e
badjok3 <dani.popoff@abv.bg>
2023-02-20 16:31:29 +0200

    Added BrowserAnimationsModule and code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { StoreModule } from '@ngrx/store';
 import { ToastrModule } from 'ngx-toastr';
 import { appRoutingModule } from './app.routing';
 
 import { AuthenticationModule } from '@authentication/authentication.module';
 import { FeedModule } from '@feed/feed.module';
 import { SharedModule } from '@shared/shared.module';
+
+import { StoreModule } from '@ngrx/store';
 import { FeedStoreModule } from '@store/feed/feed-store.module';
 
 import { AppComponent } from './app.component';
@@ -25,6 +27,7 @@ const featureModules = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CommonModule,
     HttpClientModule,
     FeedStoreModule,
